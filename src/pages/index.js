1f64353b185bf9a20296f6b5c569af5654aa6043
deceqadmin <72313289+deceqadmin@users.noreply.github.com>
2020-10-10 16:57:05 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,3 @@
-import * as React from 'react'
-+ import { graphql } from 'gatsby'
-
-const HomePage = () => {
-  return (
-    <div>
-     subtitle
-    </div>
-  )
-}
 import React from 'react'
 import { Link, graphql } from 'gatsby'
 import Masonry from 'react-masonry-component'
